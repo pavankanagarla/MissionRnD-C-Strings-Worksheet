b452cf7f684f44bacdef1347f8d75ebb72e673c6
Pavankanagarla <Pavankanagarla@missionrnd.com>
2016-03-07 05:46:06 -0800

    Submit Commit

--- src/KthIndexFromEnd.cpp
+++ src/KthIndexFromEnd.cpp
@@ -13,7 +13,7 @@ NOTES:
 
 char KthIndexFromEnd(char *str, int K) {
 	int i;
-	if ( str == nullptr || K <= 0 || str == "" )
+	if ( str == nullptr || K < 0 || str == "" )
 		return '\0';
 	for (i = 0; str[i] != '\0'; i++);
 	return str[i - K - 1];
