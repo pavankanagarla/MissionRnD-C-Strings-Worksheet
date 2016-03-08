a28c92ec89c0d2946b60bd38f6b7a20a380ece03
Pavankanagarla <Pavankanagarla@missionrnd.com>
2016-03-07 21:27:56 -0800

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -11,16 +11,25 @@ NOTES: Don't create new string.
 */
 #include <Stdio.h>
 #include <string.h>
-
-void str_words_in_rev(char *input, int len){
+void reverse(char *str, int starting_index, int ending_index){
 	int i;
 	int j;
 	char temp;
-	for (i = 0, j = len - 1; i < j; i++, j--){
-		temp = input[i];
-		input[i] = input[j];
-		input[j] = temp;
-
+	for (i = starting_index, j = ending_index - 1; i < j; i++, j--){
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
 	}
-	
+}
+
+void str_words_in_rev(char *input, int len){
+	int starting_index = 0;
+	int ending_index;
+	reverse(input, 0, len);
+	for (ending_index = 0; ending_index <= len; ending_index++){
+		if (input[ending_index] != ' ' && input[ending_index] != '\0')
+			continue;
+		reverse(input, starting_index, ending_index);
+		starting_index = ending_index + 1;
+	}
 }
